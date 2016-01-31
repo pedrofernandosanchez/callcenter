516fba8cb5f9db0f768ff4ac31881ff9836d7fa0
usuario <usuario@acer>
2016-01-30 23:34:58 -0600

    nueva clase Persona. instancia de una lista de Persona. mostrar en pantalla los elementos de la lista

--- src/pruebagit/PruebaGit.java
+++ src/pruebagit/PruebaGit.java
@@ -1,10 +1,31 @@
 package pruebagit;
 
+import com.pruebagit.classes.Persona;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.JOptionPane;
 
 public class PruebaGit {
 
     public static void main(String[] args) {
+
+        List<Persona> personas = new ArrayList<>();
+
+        personas.add(new Persona("pedro", "sanchez", 22));
+        personas.add(new Persona("elver", "gonzales", 25));
+        personas.add(new Persona("nicolas", "cage", 24));
+
+        for (Persona p : personas) {
+            String info = "nombre: "
+                    + p.getNombre()
+                    + " "
+                    + "apellido: "
+                    + p.getApellido()
+                    + " "
+                    + "edad: "
+                    + p.getEdad().toString();
+            JOptionPane.showMessageDialog(null, info);
+        }
 
         JOptionPane.showMessageDialog(null, "hola mundo");
     }
