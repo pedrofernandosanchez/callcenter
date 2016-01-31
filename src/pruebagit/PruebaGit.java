015e77afb9ce0452a9c9abaece06c00a75db9840
usuario <usuario@acer>
2016-01-30 23:24:50 -0600

    primer commit, hola mundo

---
+++ src/pruebagit/PruebaGit.java
@@ -0,0 +1,12 @@
+package pruebagit;
+
+import javax.swing.JOptionPane;
+
+public class PruebaGit {
+
+    public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null, "hola mundo");
+    }
+
+}
